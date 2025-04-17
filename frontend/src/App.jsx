8d7ee77a2b6d065d8209149e69804e635ce547dc
Vaisakh <vaisakhpn78@gmail.com>
2025-04-17 10:45:59 +0530

    Created route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Shops from "./pages/Shops";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import MyProfile from "./pages/MyProfile";
+import MyBookings from "./pages/MyBookings";
+import Booking from "./pages/Booking";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="mx-4 sm:mx-[10%]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shops" element={<Shops />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/booking/:docId" element={<Booking />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
