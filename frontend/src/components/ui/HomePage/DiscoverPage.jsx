0063bd5bdafc78ae71b2205ed0bddfa6c03d843d
Vaisakh <vaisakhpn78@gmail.com>
2025-04-20 23:58:10 +0530

    discover page added

---
+++ frontend/src/components/ui/HomePage/DiscoverPage.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const DiscoverPage = () => {
+  return (
+    <div className="flex items-center justify-center mt-16 mb-10  ">
+      <div className="flex flex-col items-start justify-center  gap-16 w-full">
+        <p className="text-black font-light text-5xl">
+          Discover Top-Rated Salons Near you
+        </p>
+        <div className="flex flex-row gap-10">
+          <div className="flex flex-col gap-3 max-w-lg">
+            <p className="text-3xl font-light text-black">
+              Find the Perfect Match
+            </p>
+            <p className="text-md font-light text-black ">
+              Search for salons based on your preferences, including
+              location,service and rating
+            </p>
+          </div>
+          <div className="flex flex-col gap-3 max-w-xl">
+            <p className="text-3xl font-light text-black">
+              Explore Diverse options
+            </p>
+            <p className="text-md font-light text-black ">
+              Browse through a curated selection of top-rated saloons in your
+              area, from hair salons to nail salons and spas
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DiscoverPage;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
+import DiscoverPage from "../components/ui/HomePage/DiscoverPage";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <DiscoverPage />
     </div>
   );
 };
