f59521aa1bace9178c7a5cc95701677f560f6c24
Vaisakh <vaisakhpn78@gmail.com>
2025-05-02 21:10:56 +0530

    Database connected

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDB from "./config/mongodb.js";
 
 // app config
 
 const app = express();
 const port = process.env.PORT || 4000;
+connectDB();
 
 // middlewares
 app.use(express.json());
