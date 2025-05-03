815e7dc6e645450f941a565140fccb0e4b737578
Vaisakh <vaisakhpn78@gmail.com>
2025-05-04 00:22:28 +0530

    Added context  and assets

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-
+import Login from "./pages/login";
 const App = () => {
-  return <div>
-    
-  </div>;
+  return (
+    <div>
+      <Login />
+    </div>
+  );
 };
 
 export default App;
---
+++ admin/src/context/ShopContext.jsx
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+
+export const ShopContext = createContext();
+
+const ShopContextProvider = (props) => {
+  const value = {};
+
+  return (
+    <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
+  );
+};
+
+export default ShopContextProvider;
