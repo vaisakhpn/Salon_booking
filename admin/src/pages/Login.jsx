a19fd5c54f6dcf56c5543013d902e3813325cd88
Vaisakh <vaisakhpn78@gmail.com>
2025-05-04 00:43:14 +0530

    created admin login ui

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -2,21 +2,58 @@ import React, { useState } from "react";
 import { assets } from "../assets/assets";
 
 const Login = () => {
+  const [state, setState] = useState("Admin");
 
-    const [state,setState]=useState('Admin')
-
-  return(
-    <form>
-        <div>
-            <p>
-                <span>
-                    {state}
-                </span>
-                Login
-            </p>
+  return (
+    <form className="min-h-[80vh] flex items-center">
+      <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-lg">
+        <p className="text-2xl gap-3 font-semibold m-auto">
+          <span className="text-blue-500"> {state} </span>
+          Login
+        </p>
+        <div className="w-full">
+          <p>Email</p>
+          <input
+            className="border border-[#DADADA] rounded w-full p-2 mt-1"
+            type="email"
+            required
+          />
         </div>
+        <div className="w-full">
+          <p>Password</p>
+          <input
+            className="border border-[#DADADA] rounded w-full p-2 mt-1"
+            type="password"
+            required
+          />
+        </div>
+        <button className="bg-blue-500 text-white w-full py-2 rounded-md text-base">
+          Login
+        </button>
+        {state === "Admin" ? (
+          <p>
+            Shop Login?{" "}
+            <span
+              className="text-blue-500 underline cursor-pointer"
+              onClick={() => setState("Shop")}
+            >
+              Click here
+            </span>
+          </p>
+        ) : (
+          <p>
+            Admin Login?{" "}
+            <span
+              className="text-blue-500 underline cursor-pointer"
+              onClick={() => setState("Admin")}
+            >
+              Click here
+            </span>
+          </p>
+        )}
+      </div>
     </form>
-  )
+  );
 };
 
 export default Login;
