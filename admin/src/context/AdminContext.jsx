ba1548cc6816c9c016c31a5dacd2a43a1ae69371
Vaisakh <vaisakhpn78@gmail.com>
2025-05-04 21:46:36 +0530

    Created Admin login

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -1,18 +1,25 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-export const AdminContext = createContext()
+export const AdminContext = createContext();
 
-const AdminContextProvider = (props) =>{
+const AdminContextProvider = (props) => {
+  const [aToken, setAToken] = useState(
+    localStorage.getItem("aToken") ? localStorage.getItem("aToken") : ""
+  );
 
-    const value ={
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
-    }
+  const value = {
+    aToken,
+    setAToken,
+    backendUrl,
+  };
 
-    return (
-        <AdminContext.Provider value={value}>
-            {props.children}
-        </AdminContext.Provider>
-    )
-}
+  return (
+    <AdminContext.Provider value={value}>
+      {props.children}
+    </AdminContext.Provider>
+  );
+};
 
-export default AdminContextProvider
+export default AdminContextProvider;
--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,11 +1,36 @@
-import React, { useState } from "react";
-import { assets } from "../assets/assets";
+import React, { useContext, useState } from "react";
+import { AdminContext } from "../context/AdminContext";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Login = () => {
   const [state, setState] = useState("Admin");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { setAToken, backendUrl } = useContext(AdminContext);
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+    try {
+      if (state === "Admin") {
+        const { data } = await axios.post(backendUrl + "/api/admin/login", {
+          email,
+          password,
+        });
+        if (data.success) {
+          localStorage.setItem("aToken", data.token);
+          setAToken(data.token);
+        } else {
+          toast.error(data.message);
+        }
+      } else {
+      }
+    } catch (error) {}
+  };
 
   return (
-    <form className="min-h-[80vh] flex items-center">
+    <form onSubmit={onSubmitHandler} className="min-h-[80vh] flex items-center">
       <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-lg">
         <p className="text-2xl gap-3 font-semibold m-auto">
           <span className="text-blue-500"> {state} </span>
@@ -14,6 +39,8 @@ const Login = () => {
         <div className="w-full">
           <p>Email</p>
           <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
             className="border border-[#DADADA] rounded w-full p-2 mt-1"
             type="email"
             required
@@ -22,6 +49,8 @@ const Login = () => {
         <div className="w-full">
           <p>Password</p>
           <input
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
             className="border border-[#DADADA] rounded w-full p-2 mt-1"
             type="password"
             required
