4388d99888935069e6fddad16dc33b47aa35de1f
Vaisakh <vaisakhpn78@gmail.com>
2025-05-07 00:40:39 +0530

    Added Shop list component in admin panel

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -1,4 +1,6 @@
+import axios from "axios";
 import { createContext, useState } from "react";
+import { toast } from "react-toastify";
 
 export const AdminContext = createContext();
 
@@ -8,11 +10,51 @@ const AdminContextProvider = (props) => {
   );
 
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const [shops, setShops] = useState([]);
+
+  const getAllShops = async () => {
+    try {
+      const { data } = await axios.post(
+        backendUrl + "/api/admin/all-shops",
+        {},
+        { headers: { aToken } }
+      );
+      if (data.success) {
+        setShops(data.shops);
+        console.log(data.shops);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const changeAvailability = async (shopId) => {
+    try {
+      const { data } = await axios.post(
+        backendUrl + "/api/admin/change-availability",
+        { shopId },
+        { headers: { aToken } }
+      );
+      if (data.success) {
+        toast.success(data.message);
+        getAllShops()
+      }else{
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
 
   const value = {
     aToken,
     setAToken,
     backendUrl,
+    shops,
+    getAllShops,
+    changeAvailability
   };
 
   return (
--- admin/src/pages/Admin/ShopList.jsx
+++ admin/src/pages/Admin/ShopList.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useContext, useEffect } from "react";
+import { AdminContext } from "../../context/AdminContext";
 
 const ShopList = () => {
-  return (
-    <div>ShopList</div>
-  )
-}
+  const { shops, aToken, getAllShops,changeAvailability } = useContext(AdminContext);
 
-export default ShopList
+  useEffect(() => {
+    if (aToken) {
+      getAllShops();
+    }
+  }, [aToken]);
+
+  return (
+    <div className="m-5 max-h-[90vh] overflow-y-scroll">
+      <h1 className="text-lg font-medium ">All Shops</h1>
+      <div className="w-full flex flex-wrap gap-4 pt-5 gap-y-6">
+        {shops.map((item, index) => (
+          <div
+            className="border border-indigo-200 hover:translate-y-[-10px] transition-all duration-500 rounded-xl max-w-56 overflow-hidden cursor-pointer group "
+            key={index}
+          >
+            <img
+              className="bg-indigo-50 w-56 h-40 object-cover "
+              src={item.image}
+              alt="shop image"
+            />
+            <div className="p-4 ">
+              <p className="text-neutral-800 text-lg font-medium">
+                {item.name}
+              </p>
+              <div className="flex mt-2 items-center gap-1 text-sm">
+                <input onChange={()=>changeAvailability(item._id)} type="checkbox" checked={item.available} />
+                <p>Available</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ShopList;
