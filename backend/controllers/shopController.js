e1a6750e26c62adccd27b22e1fccda6bcbd8bb48
Vaisakh <vaisakhpn78@gmail.com>
2025-05-07 00:40:14 +0530

    Added availability function

---
+++ backend/controllers/shopController.js
@@ -0,0 +1,20 @@
+import shopModel from "../models/shopModel.js";
+
+const changeAvailablity = async (req, res) => {
+  try {
+    const { shopId } = req.body;
+
+    const shopData = await shopModel.findById(shopId);
+
+    await shopModel.findByIdAndUpdate(shopId, {
+      available: !shopData.available,
+    });
+
+    res.json({ success: true, message: "Availability Changed" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { changeAvailablity };
--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -6,11 +6,13 @@ import {
 } from "../controllers/adminController.js";
 import upload from "../middlewares/multer.js";
 import authAdmin from "../middlewares/authAdmin.js";
+import { changeAvailablity } from "../controllers/shopController.js";
 
 const adminRouter = express.Router();
 
 adminRouter.post("/add-shop", authAdmin, upload.single("image"), addShop);
 adminRouter.post("/login", loginAdmin);
 adminRouter.post("/all-shops",authAdmin, allShops);
+adminRouter.post("/change-availability",authAdmin, changeAvailablity);
 
 export default adminRouter;
