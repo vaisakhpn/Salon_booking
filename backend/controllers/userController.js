c0e2cc683d3a513d166f1821b059c3fbf187fd2d
Vaisakh <vaisakhpn78@gmail.com>
2025-05-07 12:17:00 +0530

    user login api implemented

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,7 @@
 import validator from "validator";
 import bcrypt from "bcrypt";
 import userModel from "../models/userModel.js";
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
 const registerUser = async (req, res) => {
   try {
@@ -27,19 +27,39 @@ const registerUser = async (req, res) => {
       password: hashedPassword,
     };
 
-     const newUser = new userModel(userData);
-      const user=  await newUser.save();
+    const newUser = new userModel(userData);
+    const user = await newUser.save();
 
-      const token = jwt.sign({id:user._id},process.env.JWT_SECRET)
-    
-        res.json({ success: true, token,message: "User added" });
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
 
-
-
+    res.json({ success: true, token, message: "User added" });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
   }
 };
 
-export {registerUser}
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      res.json({ success: false, message: "User does not exist" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (isMatch) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid credentials" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { registerUser,loginUser };
