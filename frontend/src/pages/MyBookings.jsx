ae4ad19824112a0e037af91405f65fa082f8b193
Vaisakh <vaisakhpn78@gmail.com>
2025-05-10 12:11:15 +0530

    implemented my booking page

--- frontend/src/pages/MyBookings.jsx
+++ frontend/src/pages/MyBookings.jsx
@@ -1,8 +1,58 @@
 import React, { useContext } from "react";
 import { AppContext } from "../context/AppContext";
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useEffect } from "react";
 
 const MyBookings = () => {
-  const { shops } = useContext(AppContext);
+  const { backendUrl, token } = useContext(AppContext);
+
+  const [bookings, setBookings] = useState([]);
+
+  const months = [
+    "",
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const slotDateFormat = (slotDate) => {
+    const dateArray = slotDate.split("_");
+    return (
+      dateArray[0] + " " + months[Number(dateArray[1])] + " " + dateArray[2]
+    );
+  };
+
+  const getUserBooking = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/user/bookings", {
+        headers: { token },
+      });
+      if (data.success) {
+        setBookings(data.bookings.reverse());
+        console.log(data.bookings);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (token) {
+      getUserBooking();
+    }
+  }, [token]);
 
   return (
     <div>
@@ -10,24 +60,30 @@ const MyBookings = () => {
         My Bookings
       </p>
       <div>
-        {shops.slice(0, 2).map((item, index) => (
+        {bookings.slice(0, 2).map((item, index) => (
           <div
             className="grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b"
             key={index}
           >
             <div>
-              <img className="w-32 bg-indigo-50" src={item.image} alt="image" />
+              <img
+                className="w-32 bg-indigo-50"
+                src={item.shopData.image}
+                alt="image"
+              />
             </div>
             <div className="flex-1 text-sm text-zinc-600">
-              <p className="text-neutral-800 font-semibold">{item.name}</p>
+              <p className="text-neutral-800 font-semibold">
+                {item.shopData.name}
+              </p>
               <p className="text-zinc-700 font-medium mt-1">Address:</p>
-              <p className="text-xs">{item.address.line1}</p>
-              <p className="text-xs">{item.address.line2}</p>
+              <p className="text-xs">{item.shopData.address.line1}</p>
+              <p className="text-xs">{item.shopData.address.line2}</p>
               <p className="text-xs mt-1">
                 <span className="text-sm text-neutral-700 font-medium">
                   Date & Time :
                 </span>
-                25, April, 2025 | 10.30 PM
+                {slotDateFormat(item.slotDate)} | {item.slotTime}
               </p>
             </div>
             <div></div>
