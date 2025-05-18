49eaf1da3c26af2a751b75980b4f0c537ba3b2b7
Vaisakh <vaisakhpn78@gmail.com>
2025-05-18 12:15:12 +0530

    modified

--- backend/middlewares/authUser.js
+++ backend/middlewares/authUser.js
@@ -6,7 +6,7 @@ const authUser = async (req, res, next) => {
     const authHeader = req.headers.authorization;
 
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      console.log("❌ No token or wrong format:", authHeader);
+      console.log(" No token or wrong format:", authHeader);
       return res.status(401).json({
         success: false,
         message: "Not Authorized, no token provided",
@@ -14,14 +14,13 @@ const authUser = async (req, res, next) => {
     }
 
     const token = authHeader.split(" ")[1];
-    console.log("🔐 Received token:", token);
 
     const token_decode = jwt.verify(token, process.env.JWT_SECRET);
     req.user = { id: token_decode.id };
 
     next();
   } catch (error) {
-    console.log("❌ Token verification failed:", error.message);
+    console.log(" Token verification failed:", error.message);
     res.status(401).json({ success: false, message: "Token is not valid" });
   }
 };
